a13dfc879fb7bcb667685c153972f70df5cf024a
fadhilsaheer <fadhilsaheer@gmail.com>
2022-09-05 18:00:04 +0530

    base setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
